79995d9d58831e20393b5d45f00f348c5ee5fd61
sasaki <skyo.97.admj@gmail.com>
2021-06-13 11:04:21 +0900

    "hello world" will be displayed (http://localhost:3000)

---
+++ api/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Load the package
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+// Read the ports from the process.env file
+const PORT = process.env.PORT || 3000;
+
+// Load the access token and channel secret from the .env file
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// Instantiate
+const app = express();
+const client = new line.Client(config);
+
+// Do routing
+// Testing Routing
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log('http://localhost:3000');
+});
